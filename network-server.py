73b16e7d6cb4d27c55100541b6dfcb07a950c3c7
Kris Sum <kris@switchsystems.co.uk>
2017-09-22 09:36:28 +0100

    Made network server work with multiple joysticks

--- network-server.py
+++ network-server.py
@@ -1,52 +1,70 @@
 import pyvjoy
 import socket
 
-#Pythonic API, item-at-a-time
+# setup the joystick devices
 
-j = pyvjoy.VJoyDevice(1)
-j.reset()
-j.reset_buttons()
-j.reset_povs()
+vjoyDevices = { }
+
+for i in range (1,5):
+    try:
+        vjoyDevices[i] = pyvjoy.VJoyDevice(i)
+        vjoyDevices[i].reset()
+        vjoyDevices[i].reset_buttons()
+        vjoyDevices[i].reset_povs()
+        print("Joystick device " + str(i) + " found")
+    except:
+        print("Joystick device " + str(i) + " not found")
+
+# listen to network
 
 sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
 sock.bind(("0.0.0.0", 5001))
 
+print("Server listening on port 5001")
+
 def handleResponse( dataString ):
     dataString = data.decode("utf-8")
     parts = dataString.split(',')
 
-    if parts[0] == "button":
+    joystickId = int(parts[0])
 
-        buttonNumber = int(parts[1])
-        if parts[2] == "1":
+    if (not (joystickId in vjoyDevices)):
+        print ("Joystick " + str(joystickId) + " not available")
+        return
+
+    j = vjoyDevices[joystickId]
+
+    if parts[1] == "button":
+        buttonNumber = int(parts[2])
+        if parts[3] == "1":
             buttonOn = 1
         else:
             buttonOn = 0
 
         j.set_button(buttonNumber, buttonOn)
         return
-    if parts[0] == "axis":
 
-        if parts[1] == "x":
+    if parts[1] == "axis":
+        if parts[2] == "x":
             axis = pyvjoy.HID_USAGE_X
-        if parts[1] == "y":
+        if parts[2] == "y":
             axis = pyvjoy.HID_USAGE_Y
-        if parts[1] == "z":
+        if parts[2] == "z":
             axis = pyvjoy.HID_USAGE_Z
 
-        if parts[2] == "1":
+        if parts[3] == "1":
             value = 0x8000        
-        elif parts[2] == "-1":
+        elif parts[3] == "-1":
             value = 0x1
         else:
             value = 0x4000
 
         j.set_axis(axis, value)
 
-
     return
 
+# start listening loop
 
 while True: 
     data, addr = sock.recvfrom(1024)
